e89da1fed7a26076363706c947e4771d66afe3e4
DaveSugrue <dave.sugrue@gmail.com>
2017-03-07 23:18:36 +0000

    Ds - Updaing DB User table name to appUser and adding unit tests

--- src/main/java/ie/sugrue/service/login/LoginServiceImpl.java
+++ src/main/java/ie/sugrue/service/login/LoginServiceImpl.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import ie.sugrue.domain.ResponseWrapper;
-import ie.sugrue.domain.Status;
 import ie.sugrue.domain.User;
 import ie.sugrue.repository.UserRepository;
 
@@ -23,14 +23,20 @@ public class LoginServiceImpl implements LoginService {
 	@Override
 	public ResponseWrapper login(ResponseWrapper resp, User user) {
 
-		User storedUser = userRepo.getUser(user.getEmail());
+		try {
+			User storedUser = userRepo.getUser(user.getEmail());
 
-		if (validateLogin(user.getPw(), storedUser.getPw())) {
-			resp.addObject(storedUser);
-		} else {
-			Status status = new Status(1, "Username or Password are incorrect. Please try again");
-			resp.setStatus(status);
-			resp.addObject(user);
+			if (validateLogin(user.getPw(), storedUser.getPw())) {
+				resp.addObject(storedUser);
+			} else {
+				resp.getStatus().updateStatus(1, "Username or Password are incorrect. Please try again");
+				resp.addObject(user);
+			}
+		} catch (EmptyResultDataAccessException empty) {
+			resp.getStatus().updateStatus(1, "User does not exist. Please try again");
+		} catch (Exception e) {
+			log.error("Unknown exception encountered trying to log {} in.", user, e);
+			resp.getStatus().updateStatus(2, "We encountered a problem logging you in. Please try again.");
 		}
 
 		return resp;
--- src/test/java/ie/sugrue/service/user/CreateUserServiceImplTest.java
+++ src/test/java/ie/sugrue/service/user/CreateUserServiceImplTest.java
@@ -8,42 +8,56 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import ie.sugrue.domain.ResponseWrapper;
 import ie.sugrue.domain.User;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/test/resources/test-context.xml")
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/test/resources/application-context.xml")
 @Rollback
+@SpringBootTest
 public class CreateUserServiceImplTest {
 
-	private CreateUserService	createUser;
+	@Autowired
+	CreateUserService			createUserServiceImpl;
+	@Autowired
 	private ResponseWrapper		resp;
-	private final Logger		log	= LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	protected JdbcTemplate		jdbc;
+
+	private final Logger		log							= LoggerFactory.getLogger(this.getClass());
+
+	private static final String	CREATE_USER_TABLE_SCRIPT	= "scripts/create/userCreate.sql";
+	private static final String	DROP_USER_TABLE_SCRIPT		= "scripts/drop/userDrop.sql";
 
 	@Before
 	public void setUp() throws Exception {
-		createUser = new CreateUserServiceImpl();
-		resp = new ResponseWrapper();
+		ScriptUtils.executeSqlScript(jdbc.getDataSource().getConnection(), new ClassPathResource(CREATE_USER_TABLE_SCRIPT));
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		createUser = null;
+		createUserServiceImpl = null;
 		resp = null;
 	}
 
 	@Test
 	@Transactional
+	@Rollback
 	public void testCreateUser() {
 		User user = new User(1l, "Mike", "Cleary", "1981-01-01", "mike99@cleary.net", "111111");
-		resp = createUser.createUser(resp, user);
+		resp = createUserServiceImpl.createUser(resp, user);
 		assertEquals(0, resp.getStatus().getCode());
-		resp = createUser.createUser(resp, user);
+		resp = createUserServiceImpl.createUser(resp, user);
 		assertEquals(1, resp.getStatus().getCode());
 		assertEquals("The email address 'mike99@cleary.net' is already in use.", resp.getStatus().getMessages().get(0));
 	}
