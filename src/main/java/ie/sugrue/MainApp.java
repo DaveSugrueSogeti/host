5374d91bbb7fb714d1edbf1ffa64bc9dd40b4aa1
Dave Sugrue <dave.sugrue@sogeti.com>
2017-07-04 11:54:48 +0100

    Connected to gateway

--- src/main/java/ie/sugrue/MainApp.java
+++ src/main/java/ie/sugrue/MainApp.java
@@ -1,7 +1,14 @@
 package ie.sugrue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,9 +18,11 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import ie.sugrue.domain.Movie;
 import ie.sugrue.domain.Tester1;
 import ie.sugrue.domain.User;
 import ie.sugrue.repository.MySQLUserRepositoryImpl;
+import ie.sugrue.utils.Utils;
 
 @EnableAutoConfiguration
 @Configuration
@@ -25,6 +34,8 @@ public class MainApp extends SpringBootServletInitializer implements CommandLine
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MainApp.class);
 	}
+	
+	private final Logger	log	= LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(MainApp.class);
@@ -37,6 +48,12 @@ public class MainApp extends SpringBootServletInitializer implements CommandLine
 
 		System.out.println("------Starting Up--------");
 
+		callGateway();
+		
+		List<Movie> movies= populateMovies();
+		
+		displayMovies(movies);
+		
 		// ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
 
 		// MySQLUserRepositoryImpl mySQLUserRepositoryImpl = (MySQLUserRepositoryImpl) context.getBean("MySQLUserRepositoryImpl");
@@ -46,84 +63,78 @@ public class MainApp extends SpringBootServletInitializer implements CommandLine
 		// runAbstracts();
 	}
 
-	public void runAbstracts() {
-		Tester1 t1a = new Tester1();
-		Tester1 t1b = new Tester1();
+	
+	
+	public List<Movie> populateMovies(){
+		
+		List<Movie> movies = new ArrayList<Movie>();
+		
+		return movies;
+	}
+	
+	public void callGateway(){
+		
+		String urlString = "http://localhost:8080/rest/default/direct/v1/TestResource";
+		HttpURLConnection conn = null;
+		BufferedReader br = null;
+		
+		try{
+			URL url = new URL(urlString);
+			log.info("URL {} created", url);
+			
+			conn = (HttpURLConnection) url.openConnection();
+			
+			conn.setRequestMethod("GET");
+			log.info("Success - Set Request Method");
+			conn.setRequestProperty("Accept", "application/json");
+			log.info("Success - Set Accept property");
+			conn.setRequestProperty("Authorization", "Basic YWRtaW46UGFzc3dvcmQx");
+			log.info("Success - Set Authorization method");
 
-		t1a.tryThis();
-		t1a.tryThis();
-		t1a.tryThis();
-		t1a.doExtra();
-		t1b.tryThis();
-
-		t1a.met1();
-		t1a.met2();
-		t1a.met3();
-		t1a.met4();
-
+			log.info("Connection opened. Response Code = {} ", conn.getResponseCode());
+			
+			if (conn.getResponseCode() == 200) {
+				br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			} else {
+				br = new BufferedReader(new InputStreamReader((conn.getErrorStream())));
+			}
+			
+			String output;
+			
+			System.out.println("Output from server:\n");
+			
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			
+		} catch(MalformedURLException mue){
+			log.error("MalformedURLException encountered trying to specify {} as a URL", urlString, mue);
+		} catch (IOException ioe) {
+			log.error("Major IO Exception !!!", ioe);
+		} catch (Exception e) {
+			log.error("Generic Error Encounterd", e);
+		} finally {
+			
+			if (Utils.isNotNull(br)){
+				try{
+					br.close();
+				} catch(IOException ioe) {
+					log.error("Error closing buffered reader", ioe);
+				}
+			}
+			if (Utils.isNotNull(conn)){
+				log.info("Disconnecting");
+				conn.disconnect();
+				log.info("Disconnected");
+			}
+			
+		}
 	}
 
-	private void popUsers(MySQLUserRepositoryImpl mySQLUserRepositoryImpl) {
-		// Delete all after 1
-		System.out.println("------Records Reset--------");
-		List<User> users = mySQLUserRepositoryImpl.getUsers();
-		int numberOfRows = users.size();
-		System.out.println("There are currently " + numberOfRows + " rows.");
-		for (int i = 0; i < users.size(); i++) {
-			System.out.println("Deleting index " + i);
-			Long id = users.get(i).getId();
-			mySQLUserRepositoryImpl.deleteUser(id);
-			System.out.println("Deleted id = " + id);
+	public void displayMovies(List<Movie> movies){
+		
+		for( Movie movie : movies){
+			System.out.println(movie.getName());
 		}
-
-		System.out.println("------Records Creation--------");
-
-		User c1 = new User(0l, "Mike", "Cleary", "1985-05-01", "mike@cleary.net", "111111");
-		mySQLUserRepositoryImpl.createUser(c1);
-
-		User c2 = new User(0l, "Mary", "Cleary", "1985-05-02", "mary@cleary.net", "222222");
-		mySQLUserRepositoryImpl.createUser(c2);
-
-		User c3 = new User(0l, "Lucy", "Cleary", "1985-05-03", "lucy@cleary.net", "333333");
-		mySQLUserRepositoryImpl.createUser(c3);
-
-		User c4 = new User(0l, "John", "Cleary", "1985-05-04", "john@cleary.net", "444444");
-		mySQLUserRepositoryImpl.createUser(c4);
-
-		User c5 = new User(0l, "Anne", "Cleary", "1985-05-05", "anne@cleary.net", "555555");
-		mySQLUserRepositoryImpl.createUser(c5);
-
-		User c6 = new User(0l, "Paul", "Cleary", "1985-05-06", "paul@cleary.net", "666666");
-		mySQLUserRepositoryImpl.createUser(c6);
-
-		User c7 = new User(0l, "Jack", "Cleary", "1985-05-07", "jack@cleary.net", "777777");
-		mySQLUserRepositoryImpl.createUser(c7);
-
-		User c8 = new User(0l, "Lara", "Cleary", "1985-05-08", "lara@cleary.net", "888888");
-		mySQLUserRepositoryImpl.createUser(c8);
-
-		User c9 = new User(0l, "Dave", "Sugrue", "1985-05-09", "dave.sugrue@gmail.com", "888888");
-		mySQLUserRepositoryImpl.createUser(c9);
-
-		System.out.println("------Listing Multiple Records--------");
-		users = mySQLUserRepositoryImpl.getUsers();
-		for (User record : users) {
-			System.out.print("ID : " + record.getId());
-			System.out.print(", First Name : " + record.getFirstName());
-			System.out.println(", Last Name : " + record.getLastName());
-		}
-
-		User userTobBeUpdate = users.get(6);
-		userTobBeUpdate.setPw("999999");
-		System.out.println("----Updating Record with ID = " + userTobBeUpdate.getId() + " -----");
-		mySQLUserRepositoryImpl.updateUser(userTobBeUpdate);
-
-		System.out.println("----Updated Record with ID = " + userTobBeUpdate.getId() + " -----");
-		User user = mySQLUserRepositoryImpl.getUser(userTobBeUpdate.getId());
-		System.out.print("ID : " + user.getId());
-		System.out.print(", First Name : " + user.getFirstName());
-		System.out.println(", Last Name : " + user.getLastName());
-
-		System.out.println("#####     AND, WE'RE DONE !!!     #####");
 	}
 }
